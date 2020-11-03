636a46054d283d7c2e1bbf34b7f660a01cc3a65a
Neil M <n.mckenzie@live.com>
2020-11-03 12:09:31 -0600

    added password encryption

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,7 @@ passport.use(new LocalStrategy(
       }
       // If there is a user with the given email, but the password the user gives us is incorrect
       // else if (!dbUser.validPassword(password)) {
-        else if (password !== dbUser.password){
+        else if (!dbUser.validPassword(password)){
         return done(null, false, {
           message: "Incorrect password."
         });
